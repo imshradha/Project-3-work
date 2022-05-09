d45ad4f941d479a08deeefea20a4d52150946091
amit token <amitbabhanchak@gmail.com>
2022-05-09 23:53:56 +0530

    phase1complete

---
+++ src/Validator/valid.js
@@ -0,0 +1,8 @@
+
+const isValid = function(value) {
+    if(typeof (value) == "undefined" || typeof (value) == null) {return false}
+    if(typeof (value).trim().length == 0){ return false}
+    if(typeof (value) == "string" && (value).trim().length > 0) {return true}
+}
+
+module.exports.isValid = isValid
