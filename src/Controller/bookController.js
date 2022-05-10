93100861009faa47dda708cdbc8177d35f6ed364
DeeepshikhaRaghuvanshi <ddeepshikha.raghuvanshi@gmail.com>
2022-05-10 14:58:01 +0530

    minor changes in POST/books

--- src/Controller/bookController.js
+++ src/Controller/bookController.js
@@ -13,16 +13,21 @@ const Book = async function (req, res) {
          //check title is valid or not
         if(!Validator.isValid(title)) return res.status(400).send({status: false,message: "Title is Required"});
         if(!/^[A-Za-z ]+$/.test(title))  return res.status(400).send({status: false, message: "Invalid title"});
+        let titleCheck = await bookModel.findOne({title : title})
+        if(titleCheck) return res.status(400).send({status : false , message :" title already exists , Enter unique value"})
 
          //check excerpt is valid or not
         if(!Validator.isValid(excerpt)) return res.status(400).send({status: false,message: "Excerpt is Required"});
 
         //check userId is valid or not
         if(!Validator.isValid(userId)) return res.status(400).send({status: false,message: "userId is Required"});
-        if(Validator.isValidObjectId(userId))  return res.status(400).send({status: false,message: "userId is Required"});
+        if(!Validator.isValidObjectId(userId))  return res.status(400).send({status: false,message: "userId is invalid"});
+       
 
         //check ISBN is valid or not
         if(!Validator.isValid(ISBN)) return res.status(400).send({status: false,message: "ISBN is Required"});
+        let userIdCheck = await bookModel.findOne({ISBN : ISBN})
+        if(userIdCheck) return res.status(400).send({status : false , message :" ISBN already exists , Enter unique value"})
 
         //check category is valid or not
         if(!Validator.isValid(category)) return res.status(400).send({status: false,message: "category is Required"});
@@ -31,7 +36,7 @@ const Book = async function (req, res) {
         if(!Validator.isValid(subcategory)) return res.status(400).send({status: false,message: "subcategory is Required"});
 
         let savedData = await bookModel.create(data)
-        return res.status(201).send({ status: true, data: savedData});
+        return res.status(201).send({ status: true, message : "success", data: savedData});
     }
     catch(err){
         return res.status(500).send({status : false, message: err.message});
