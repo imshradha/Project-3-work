0d449d26db0581f13f379941aa14af820093254e
DeeepshikhaRaghuvanshi <ddeepshikha.raghuvanshi@gmail.com>
2022-05-16 15:48:25 +0530

    changes is package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const route = require("./routes/route");
-const { default: mongoose } = require('mongoose');
+const  mongoose  = require('mongoose');
 const app = express();
 
 app.use(express.json());
